8bc94b7cf62e8bf7f8e407545c715bf43718a79a
czcz1024@google.com <czcz1024@gmail.com>
2014-07-01 23:27:20 +0800

    fix scripts floder

--- Poker24/app/app.js
+++ Poker24/app/app.js
@@ -7,53 +7,52 @@ var app = {
 	apiBaseUrl:"http://221.204.241.69:99/"
 };
 
-$(function () {
-    //document.domain = app.apiBaseUrl;
-    start();
-});
+//$(function () {
+//    //document.domain = app.apiBaseUrl;
+//    start();
+//});
 
-function start() {
-    //localStorage.clear();
-    var server = localStorage.getItem("server");
-    if (server != null) {
-        app.apiBaseUrl = server;
-    } else {
-        localStorage.setItem("server", app.apiBaseUrl);
-    }
-
-    if (typeof (init) == "function") {
-        init();
+//function start() {
+//    //localStorage.clear();
+//    var server = localStorage.getItem("server");
+//    if (server != null) {
+//        app.apiBaseUrl = server;
+//    } else {
+//        localStorage.setItem("server", app.apiBaseUrl);
+//    }
+//    alert("typeof :"+typeof (init));
+//    if (typeof (init) == "function") {
+//        init();
         
-    }
+//    }
     
-}
+//}
 
 function quit() {
     localStorage.clear();
     location.href = "login.html";
 }
 
-(function () {
-    "use strict";
+//(function () {
+//    "use strict";
 
-    document.addEventListener('deviceready', onDeviceReady.bind(this), false);
+//    document.addEventListener('deviceready', onDeviceReady.bind(this), false);
 
-    function onDeviceReady() {
-        // Handle the Cordova pause and resume events
+//    function onDeviceReady() {
+//        // Handle the Cordova pause and resume events
         
-        document.addEventListener('pause', onPause.bind(this), false);
-        document.addEventListener('resume', onResume.bind(this), false);
-        start();
+//        document.addEventListener('pause', onPause.bind(this), false);
+//        document.addEventListener('resume', onResume.bind(this), false);
+//        start();
 
+//        // TODO: Cordova has been loaded. Perform any initialization that requires Cordova here.
+//    };
 
-        // TODO: Cordova has been loaded. Perform any initialization that requires Cordova here.
-    };
+//    function onPause() {
+//        // TODO: This application has been suspended. Save application state here.
+//    };
 
-    function onPause() {
-        // TODO: This application has been suspended. Save application state here.
-    };
-
-    function onResume() {
-        // TODO: This application has been reactivated. Restore application state here.
-    };
-})();
+//    function onResume() {
+//        // TODO: This application has been reactivated. Restore application state here.
+//    };
+//})();
